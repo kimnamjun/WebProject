0b76ab32976cf9f908f814930c728b7c1a011a97
kimnamjun <storypass@naver.com>
2020-12-14 14:04:40 +0900

    Update hidden_data.js

--- web/hidden_data.js
+++ web/hidden_data.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mysql_password: 'qudtls59!',
+    mysql_password: 'PASSWORD',
     encryption_key: 'qkfrguwlwldksgdmsdkaghzl',
     session_secret: 'Enfgdmftndjqtsmsdkagh',
     password_regex: /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&]).*$/
